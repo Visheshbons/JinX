78aa13c5183f857cd91cc5533aed4a94b667dd27
Jins Niravel Syamchand <that1developeroffical@gmail.com>
2026-05-06 21:11:35 +1200

    pushed

--- JinX_Virt/Lexer.hpp
+++ JinX_Virt/Lexer.hpp
@@ -3,7 +3,7 @@
 #include <string>
 
 enum TokenType {
-    T_INT, T_IDENT, T_NUM, T_EQ, T_SEMI_COLON, T_EOF
+    T_INT, T_CHAR, T_BOOL, T_STR, T_FLOAT, T_IDENT, T_NUM, T_EQ, T_LB, T_RB, T_L_SQB, T_R_SQB, T_SEMI_COLON, T_EOF
 };
 
 struct Token {
@@ -24,7 +24,10 @@ std::vector<Token> Lexer(std::string_view& Source) {
             while (isalnum(Source[Iteration]))  Word += Source[Iteration++];
 
             if (Word == "int") Tokens.push_back({T_INT, Word});
-            else Tokens.push_back({T_IDENT, Word}); 
+            else if (Word == "char") Tokens.push_back({T_CHAR, Word});
+            else if (Word == "bool") Tokens.push_back({T_BOOL, Word});
+            else if (Word == "str") Tokens.push_back({T_STR, Word});
+            else if (Word == "float") Tokens.push_back({T_FLOAT, Word});
         } else if (isdigit(Character)) {
             std::string Number;
             while (isnumber(Source[Iteration])) Number += Source[Iteration++];
